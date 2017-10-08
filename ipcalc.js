0f76952a0e03d1273197602fc5cc92bb110c7d5b
stereocat <stereocat@gmail.com>
2017-10-08 21:59:28 +0900

    Debug & Refactoring

--- ipcalc.js
+++ ipcalc.js
@@ -6,41 +6,34 @@ var app = new Vue({
     data: {
         ipAddr: '192.168.0.1',
         subnet: '255.255.255.0',
-        invalidAddr: true
+        invalidAddr: false,
+        invalidMask: false
     },
     computed: {
         binIpAddr: function() {
             return this.toBin(this.ipAddr);
         },
 
-        length: function () {
-            var val = this.subnet; // String
-            if (/^\d+\.\d+\.\d+.\d+$/.test(this.subnet)) {
-                try {
-                    this.invalidAddr = false;
-                    val = ip.subnet(this.ipAddr, this.subnet).subnetMaskLength; // Number
-                }
-                catch(err) {
-                    console.log("catch error in length");
-                    this.invalidAddr = true;
-                    val = -1;
-                }
+        length: function() {
+            var val = -1; // default (error value)
+            if (this.validMaskLengthStr(this.subnet)){
+                val = Number(this.subnet);
+            } else if (this.validIPv4MaskStr(this.subnet)) {
+                val = ip.subnet('127.0.0.0', this.subnet).subnetMaskLength; // dummy ip
+            } else {
+                this.invalidMask = true;
             }
-            return Number(val); // always number
+            return val;
         },
 
-        netmask: function () {
-            var val = this.subnet;
-            if(/^\d+$/.test(this.subnet)) {
-                try {
-                    this.invalidAddr = false;
-                    val = ip.fromPrefixLen(Number(this.subnet));
-                }
-                catch(err) {
-                    console.log("catch error in netmask");
-                    this.invalidAddr = true;
-                    val = "0.0.0.0";
-                }
+        netmask: function() {
+            var val = '0.0.0.0'; // default
+            if (this.validMaskLengthStr(this.subnet)) {
+                val = ip.fromPrefixLen(Number(this.subnet));
+            } else if (this.validIPv4MaskStr(this.subnet)) {
+                val = this.subnet;
+            } else {
+                this.invalidMask = true;
             }
             return val;
         },
@@ -48,104 +41,48 @@ var app = new Vue({
             return this.toBin(this.netmask);
         },
 
-        wildcard: function () {
-            try {
-                this.invalidAddr = false;
-                return ip.not(this.netmask);
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in wildcard");
-                return "0.0.0.0";
-            }
+        wildcard: function() {
+            return ip.not(this.netmask);
         },
         binWildcard: function() {
             return this.toBin(this.wildcard);
         },
 
-        network: function () {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).networkAddress;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in network");
-                return "0.0.0.0";
-            }
+        network: function() {
+            return this.ipv4Subnet().networkAddress;
         },
         binNetwork: function() {
             return this.toBin(this.network);
         },
 
         broadcast: function() {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).broadcastAddress;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in broadcast");
-                return "0.0.0.0";
-            }
+            return this.ipv4Subnet().broadcastAddress;
         },
         binBroadcast: function() {
             return this.toBin(this.broadcast);
         },
 
         first: function() {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).firstAddress;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in first");
-                return "0.0.0.0";
-            }
+            return this.ipv4Subnet().firstAddress;
         },
         binFirst: function() {
             return this.toBin(this.first);
         },
 
         last: function() {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).lastAddress;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in last");
-                return "0.0.0.0";
-            }
+            return this.ipv4Subnet().lastAddress;
         },
         binLast: function() {
             return this.toBin(this.last);
         },
 
         numberOfHosts: function() {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).numHosts;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in numberOfHosts");
-                return -1;
-            }
+            return this.ipv4Subnet().numHosts;
         },
         numberOfAddrs: function() {
-            try {
-                this.invalidAddr = false;
-                return ip.subnet(this.ipAddr, this.netmask).length;
-            }
-            catch (err) {
-                this.invalidAddr = true;
-                console.log("catch error in numberOfAddrs");
-                return -1;
-            }
+            return this.ipv4Subnet().length;
         },
-        rows: function () {
+        rows: function() {
             return [
                 { name: "IP Address", value: this.ipAddr, binary: this.binIpAddr },
                 { name: "Subnet Length", value: this.length, binary: "" },
@@ -160,29 +97,82 @@ var app = new Vue({
             ];
         }
     },
+    watch: {
+        ipAddr: function(newIPStr) {
+            try {
+                if (this.validIPv4Str(newIPStr)) {
+                    this.invalidAddr = false;
+                    this.ipAddr = newIPStr;
+                } else {
+                    throw new Error("invalidIPAddr: " + newIPStr);
+                }
+            } catch(err) {
+                this.invalidAddr = true;
+                console.log("error in ipAddr watcher, %s", err);
+            }
+        },
+        subnet: function(newSubnetStr) {
+            try {
+                if (this.validMask(newSubnetStr)) {
+                    this.invalidMask = false;
+                    this.subnet = newSubnetStr; // string (also, if length-number)
+                } else {
+                    throw new Error("invalidMask");
+                }
+            } catch(err) {
+                this.invalidMask = true;
+                console.log("error in subnet watcher, %s", err);
+            }
+        }
+    },
     methods: {
+        validMask: function(subnetStr) {
+            return this.validMaskLengthStr(subnetStr) || this.validIPv4MaskStr(subnetStr);
+        },
+        ipv4Subnet: function() {
+            if (!this.validIPv4Str(this.ipAddr)) {
+                this.invalidAddr = true;
+                return ip.subnet('127.0.0.1', '255.255.255.0'); // dummy
+            }
+            if (!this.validMask(this.netmask)) {
+                this.invalidMask = true;
+                return ip.subnet('127.0.0.1', '255.255.255.0'); // dummy
+            }
+            return ip.subnet(this.ipAddr, this.netmask);
+        },
+        validIPv4MaskStr: function(ipv4MaskStr) {
+            return /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/.test(ipv4MaskStr);
+        },
+        validIPv4Str: function(ipv4Str) {
+            // return ip.isV4Format(ipv4Str);
+            return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipv4Str);
+        },
+        validMaskLengthStr: function(lenStr) {
+            return (/^\d+$/.test(lenStr) && Number(lenStr) >= 0 && Number(lenStr) <= 32);
+        },
         toBin: function(dottedStr) {
             try {
-                this.invalidAddr = false;
                 var buf = ip.toBuffer(dottedStr);
                 var octets = [];
-                for(var i=0; i<buf.length; i++) {
+                for (var i = 0; i < buf.length; i++) {
                     octets.push(this.paddingOctet(buf[i].toString(2)));
                 }
-                var dottedStr = octets.join('.');
-                var sep = this.length + Math.floor((this.length - 1) / 8);
-                var headStr = dottedStr.slice(0, sep);
-                var tailStr = dottedStr.slice(sep);
-                return { head: headStr, tail: tailStr };
-            }
-            catch (err) {
-                console.log("error in toBin");
-                this.invalidAddr = true;
+                var binDottedStr = octets.join('.');
+                if (this.length > 0) {
+                    var sep = this.length + Math.floor((this.length - 1) / 8);
+                    var headStr = binDottedStr.slice(0, sep);
+                    var tailStr = binDottedStr.slice(sep);
+                    return { head: headStr, tail: tailStr };
+                } else {
+                    return { head: "", tail: binDottedStr };
+                }
+            } catch (err) {
+                console.log("error in toBin, %s", err);
                 return "";
             }
         },
         paddingOctet: function(octetBinStr) {
-            return ('00000000' + octetBinStr ).slice(-8);
+            return ('00000000' + octetBinStr).slice(-8);
         }
     }
-})
+});
