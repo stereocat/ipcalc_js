3f9508511c4dd9759bd21f06c3bcb01e43d6e192
stereocat <stereocat@gmail.com>
2017-10-14 15:20:29 +0900

    fix file path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
+    entry: "./src/js/ipcalc.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "public/js")
+    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.js'
